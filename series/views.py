84925065d87abc3f54fb33d6b2e43d9474c937f1
idemku <pityu-10@hotmail.com>
2018-03-30 00:50:43 +0200

    Következő epizód megjelenési dátum hozzáadva, api_key-t most már conf.cnf-ből olvassa be.

--- series/views.py
+++ series/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
-import http.client, urllib.parse, json
+import http.client, urllib.parse, json, configparser, datetime
 
-api_key = "minta1"
-language = "hu-HU"
-page_number = 1
+config = configparser.ConfigParser()
+config.read("conf.cnf")
+if config.sections() == []:
+    print("Nem sikerült beolvasni a conf.cnf fájlt. A program leáll...")
+    exit(1)
+
+api_key = config["SW"]["apiKey"]
+language = config["SW"]["language"]
 
 
 def index(request):
     try:
         title = request.POST['title']
         if title == "":
-            context = {"name": "", "vote_average": "", "first_air_date": "", "overview": ""}
+            context = {"name": "", "vote_average": "", "first_air_date": "", "next_episode_date": "", "overview": ""}
         else:
             context = search_tv(title)
     except KeyError:
-        context = {"name": "", "vote_average": "", "first_air_date": "", "overview": ""}
+        context = {"name": "", "vote_average": "", "first_air_date": "", "next_episode_date": "", "overview": ""}
     return render(request, 'series/index.html', context)
 
 
@@ -25,8 +30,7 @@ def search_tv(title):
 
     # összefűzzük a linket a lekérdezéshez
     link = "/3/search/tv?"
-    link += urllib.parse.urlencode({"page": page_number, "query": title,
-                                    "language": language, "api_key": api_key})
+    link += urllib.parse.urlencode({"query": title, "language": language, "api_key": api_key})
 
     # meghívja a linket és a visszakapott JSON-t eltárolja a data változóban
     conn.request("GET", link, payload)
@@ -37,7 +41,34 @@ def search_tv(title):
         "name": json_data["results"][0]["name"],
         "vote_average": str(json_data["results"][0]["vote_average"]),
         "first_air_date": json_data["results"][0]["first_air_date"],
+        "next_episode_date": "Ismeretlen",
+        "last_air_date": "Ismeretlen",
         "overview": json_data["results"][0]["overview"]
     }
+    id = json_data["results"][0]["id"]
+
+    link = "/3/tv/" + str(id) + "?" + urllib.parse.urlencode({"api_key": api_key, "language": language})
+    conn.request("GET", link, payload)
+    res = conn.getresponse()
+    data = res.read()
+    json_data = json.loads(data.decode("utf-8"))
+    output_data["last_air_date"] = json_data["last_air_date"]
+    last_season_num = json_data["seasons"][len(json_data["seasons"])-1]["season_number"]
+
+    link = "/3/tv/" + str(id) + "/season/" + str(last_season_num) + "?"
+    link += urllib.parse.urlencode({"api_key": api_key, "language": language})
+    conn.request("GET", link, payload)
+    res = conn.getresponse()
+    data = res.read()
+    json_data = json.loads(data.decode("utf-8"))
+
+    today = datetime.datetime.today().strftime("%Y-%m-%d")  # formázott stringként adja vissza a dátumot
+    print(link)
+    for i in json_data["episodes"]:
+        if i["air_date"] >= today:
+            output_data["next_episode_date"] = i["air_date"]
+            break
+
+    print(str(output_data["next_episode_date"]))
 
     return output_data
